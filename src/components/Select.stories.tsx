1a430145a9e4ac97c6dbf9fa33bb8b48a2795394
Gwongusun <love93200@naver.com>
2025-12-09 17:19:24 +0900

    storybook 6

--- src/components/Select.stories.tsx
+++ src/components/Select.stories.tsx
@@ -20,8 +20,8 @@ const sampleOptions = [
   { label: '옵션 2 (Vue)', value: 'vue' },
   { label: '옵션 3 (Angular)', value: 'angular' },
   { label: '옵션 4 (React)', value: 'etc' },
-  { label: '옵션 5 (Vue)', value: 'vue' },
-  { label: '옵션 6 (Angular)', value: 'angular' },
+  { label: '옵션 5 (Vue)', value: 'etc2' },
+  { label: '옵션 6 (변경사항)', value: 'etc3' },
 ];
 
 export const Default: Story = {
